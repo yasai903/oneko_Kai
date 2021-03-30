cfaecbf06d376ce8fd23377ed8a253e19c1565d3
yasai903 <61590324+yasai903@users.noreply.github.com>
2021-03-31 00:38:49 +0800

    Add files via upload

--- CrazyJoyRevision.js
+++ CrazyJoyRevision.js
@@ -136,6 +136,7 @@ if ($.isNode()) {
   $.md5 = A
 }(this);
 !(async () => {
+    console.log(`Version 0.0.0\n\n`);
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
